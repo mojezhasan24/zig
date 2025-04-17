3ff270a4d1be217782dfe8cd0dd32a7fca37e896
mojezhasan24 <mojezhasan@outlook.com>
2025-04-17 21:22:40 +0530

    sed flaot formatting to print 11.0 instead of the std formatting that is 1.1e1

--- values.zig
+++ values.zig
@@ -6,4 +6,7 @@ const assert = std.debug.assert;
 pub fn main() void {
     const one_plus_one: i32 = 1 + 1;
     print("1 + 1 = {}\n", .{one_plus_one});
+
+    const flt_addition: f32 = 7.7 + 3.3;
+    print("7.7 + 3.3 = {d:.2}\n", .{flt_addition});
 }
